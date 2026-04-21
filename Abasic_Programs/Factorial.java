78ececee730e0a4b122e0fa9ffd89396836d7237
Aman <amanv9617@gmail.com>
2026-04-21 21:01:46 +0530

    basic programs

--- Abasic_Programs/Factorial.java
+++ Abasic_Programs/Factorial.java
@@ -1,3 +1,15 @@
+import java.util.*;
 public class Factorial {
-    
+    // 5 =5*4*3*2*1 =125
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("input the value ");
+        int x =sc.nextInt();
+        int count = 1;
+        for(int i=x;i>0;i--){
+            count =count*i;
+        }
+        System.out.println(count);
+    }
+
 }
--- Abasic_Programs/Palindrome.java
+++ Abasic_Programs/Palindrome.java
@@ -1,4 +1,24 @@
-
+import java.util.*;
+// 121, //12321
 public class Palindrome{
+public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+    int n =sc.nextInt();
+
+    int original =n;
+    int reverse= 0;
+
+    while(n>0){
+        int digit =n%10;
+        reverse =reverse*10+digit;
+        n=n/10;
+    }
+    if(original==reverse){
+System.out.println("yes");
+    }
+    else{
+        System.out.println("false");
+    }
     
+}    
 }
