232758e291cc51eac2e9926942d5ec8be2078a4a
Aman <amanv9617@gmail.com>
2026-04-21 11:55:22 +0530

    Typecasting programs

---
+++ Typecasting/Explicitly_Casting.java
@@ -0,0 +1,8 @@
+public class Explicitly_Casting {
+    public static void main(String[] args) {
+        long ls =1212L;
+        int in =(int)ls;
+
+        System.out.println("typecast :  "+in);
+    }
+}
---
+++ Typecasting/Implicitly_Casting.java
@@ -0,0 +1,15 @@
+public class Implicitly_Casting {
+    public static void main(String[] args) {
+        byte b =127;
+        int in =b;
+        long lo =in;
+        float fl=lo;
+        double db =fl;
+        System.out.println(db);
+        long a2 =12322L;
+        System.out.println(in+a2);
+
+
+    }
+
+}
