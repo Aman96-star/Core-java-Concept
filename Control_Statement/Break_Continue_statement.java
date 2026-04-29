0b6f1478a919ba83822d2d7999a8f10d278de057
Aman <amanv9617@gmail.com>
2026-04-29 13:37:35 +0530

    add files od conditional statement

---
+++ Control_Statement/Break_Continue_statement.java
@@ -0,0 +1,13 @@
+package Control_Statement;
+public class Break_Continue_statement {
+    public static void main(String[] args) {
+       int n =5;
+       for(int i=1;i<=n;i++){
+        // if(i==4)break;
+        // System.out.println(i);
+
+        if(i==2)continue;
+        System.out.println(i);
+       }
+    }
+}
