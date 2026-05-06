d150a5d3b03e63279376f1cc5a34df9f070bca59
Aman <amanv9617@gmail.com>
2026-05-06 09:42:07 +0530

    add new files

---
+++ Control_Statement/All.java
@@ -0,0 +1,218 @@
+package Control_Statement;
+import java.util.*;
+/*
+
+
+1) Write a program to find the greatest of two numbers using if-else.
+2) Write a program to find the greatest of three numbers using if-else-if.
+3) Check if a number is positive, negative, or zero.
+4) Write a program to check if a given year is a leap year.
+5) Check if a character is a vowel or consonant using switch.
+6) Check if a number is even or odd using if-else.
+7) Find the grade of a student using if-else-if (based on marks).
+8) Write a program to find the largest of three numbers using nested if.
+9) Program to check if a number is divisible by both 3 and 5.
+10) Write a calculator program using switch (add, sub, mul, div).*/
+public class All {
+   public static void main(String[] args) {
+   
+    // int a=45;
+    // int b= 30;
+
+    // if(a>b){
+    //     System.out.println(a);
+    // }
+    // else{
+    //     System.out.println(b);
+    // }
+
+    
+    // int yr=2023;
+    
+    // if(yr%400==0){
+    //     System.out.println("yes leap year");
+    // }
+    // else if(yr%4==0){
+    //     System.out.println("yes leap");
+    // }
+    // else{
+    //     System.out.println("no");
+    // }
+
+//     char ch ='m';
+
+//     if(ch=='A' || ch=='E' ||ch=='I' || ch=='O' || ch=='U'){
+//         System.out.println("yes it is");
+//     }
+
+//    else  if(ch=='a' || ch=='e' ||ch=='i' || ch=='o' || ch=='u'){
+//         System.out.println("yes it is");
+//     }
+
+//     else{
+//         System.out.println("no it is not Vowel");
+//     }
+
+    //  int n=33;
+    //  if(n%3==0 && n%5==0){
+    //     System.out.println("yes divisible y3  and 5");
+
+    //  }
+    
+    //  else{
+    //             System.out.println("not divisible  ");
+
+    //  }
+    // int a =24;
+    // int b =20;
+    // Scanner sc =new Scanner(System.in);
+    // char ch = sc.next().charAt(0);
+  
+
+    // switch(ch){
+    //     case'+':
+    //     System.out.println("Add"+(a+b));
+    //     break;
+    //     case'-':
+    //     System.out.println("minus"+(a-b));
+    //     break;
+    //     case'*':
+    //     System.out.println("pro"+(a*b));
+    //     break;
+    //     case'/':
+    //     System.out.println("div"+(a/b));
+    //     break;
+
+    //     default :
+    //     System.out.println("invalid");
+   
+    // }
+
+    // convert day into names 
+    // Scanner sc =new Scanner(System.in);
+    // int week =sc.nextInt();
+
+    // switch (week) {
+    //     case 1:
+    //         System.out.println("Monday");
+    //         break;
+    //     case 2:
+    //         System.out.println("tuesday");
+    //         break;
+    //     case 3:
+    //         System.out.println("wed");
+    //         break;
+    //     case 4:
+    //         System.out.println("thu");
+    //         break;
+    //     case 5:
+    //         System.out.println("Fri");
+    //         break;
+    //     case 6:
+    //         System.out.println("Sat");
+    //         break;
+    //     case 7:
+    //         System.out.println("Sunday");
+    //         break;
+    
+    //     default:
+    //         System.out.println("invalid num");
+    //         break;
+    // }
+
+    // String str ="hey Aman";
+    // switch (str) {
+    //     case "hey manhor":
+    //         System.out.println("yes case 1");
+    //         break;
+    //     case "hey rean":
+    //         System.out.println("yes case 2");
+    //         break;
+    //     case "hey Aman":
+    //         System.out.println("yes case 3");
+    //         break;
+    
+    //     default:
+    //         System.out.println("no where it is ");
+    //         break;
+    // }
+    // double dd =24.212;
+    
+    // switch (dd) {
+    //     case 211.1:
+    //         System.out.println("yes case 1");
+    //         break;
+    //     case 24.212:
+    //         System.out.println("yes case 2");
+    //         break;
+     
+    
+    //     default:
+    //         System.out.println("no where it is ");
+    //         break;
+    // }
+
+//     int a =1;
+//     do{
+// System.out.println( "\n" +a);
+//         a++;
+// }
+//     while(a<=5);
+    
+// Scanner ob =new Scanner(System.in);
+// int num =ob.nextInt();
+   
+//     int sum=0;
+
+//     while(num>0){
+//         int digit=num%10;     
+//         sum+=digit*digit*digit;
+//         num=num/10;
+//     }
+//     System.out.println(sum);
+//     int n =153;
+//     int sum=0;
+//     while(n>0){
+//        int  digit= n%10;  //3
+//        sum+=digit*digit*digit;  //27+
+//        n= n/10;
+//     }
+//         System.out.println(sum);
+        // int n =145;
+        // int sumfact =0;
+        // while(n>0){
+        //     int digit =n%10;
+        //     int fact =1;
+        //     for(int i=digit;i>0;i--){
+        //         fact =fact*i;
+        //     }
+        //     sumfact+=fact;
+        //     n=n/10;
+
+        // }
+        // System.out.println(sumfact);
+        // int n=  28;
+        // int sum =0;
+        // for(int i=1;i<=n/2;i++){
+        //    if(n%i==0){
+        //     sum+=i;
+        //    }
+            
+        // }
+        // System.out.println(sum);
+        // int n =2121;
+        // int rev=0;
+        // while(n>0){
+        //     int digit=n%10;
+        //     rev=rev*10+digit;
+        //     n=n/10;
+        // }
+
+        // System.out.println(rev);
+        
+        //pa
+
+   }
+}
+
+
