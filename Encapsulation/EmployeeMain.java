cb04277de2b6834d943bc9f952c8a450961b388e
Aman <amanv9617@gmail.com>
2026-05-19 10:22:08 +0530

    add all 4 pillar of oops

---
+++ Encapsulation/EmployeeMain.java
@@ -0,0 +1,24 @@
+package Encapsulation;
+
+public class EmployeeMain {
+    public static class Employee{
+        private int salary =2000;
+
+        public void setSalary(int salary){
+            this.salary =salary;
+        }
+
+        public int  getSalary(){
+            return salary;
+        }
+    }
+
+    public static void main(String[] args) {
+        Employee ee = new Employee();
+    System.out.println(ee.getSalary());
+    
+    ee.setSalary(10000);
+    System.out.println(ee.getSalary());
+
+    }
+}
