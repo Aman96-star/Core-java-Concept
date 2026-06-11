b62dc219e0b1ba3724bd8ddc575ba92e27c2ffdc
Aman <amanv9617@gmail.com>
2026-06-11 10:32:50 +0530

    add collection topics

---
+++ COLLECTION_FRAMEWORK/Generics.java
@@ -0,0 +1,17 @@
+package COLLECTION_FRAMEWORK;
+
+import java.util.ArrayList;
+
+public class Generics {
+    public static void main(String[] args) {
+        // ArrayList list =new ArrayList();
+        // list.add("Aman");
+        // list.add("true");
+        
+        ArrayList<String> list = new ArrayList<>();
+
+         
+        // String name =(String)list.get(0);
+        // System.out.println(name);
+    }
+}
---
+++ COLLECTION_FRAMEWORK/ListDuplicate.java
@@ -0,0 +1,12 @@
+package COLLECTION_FRAMEWORK;
+
+import java.util.ArrayList;
+
+public class ListDuplicate {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(24);
+        list.add(24);
+        System.out.println(list);
+    }
+}
