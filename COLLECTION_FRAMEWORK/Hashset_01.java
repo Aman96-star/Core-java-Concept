52113f2c282f30b11e5ef836660cb5e78477e293
Aman <amanv9617@gmail.com>
2026-06-26 09:06:11 +0530

    add files

---
+++ COLLECTION_FRAMEWORK/Hashset_01.java
@@ -0,0 +1,7 @@
+package COLLECTION_FRAMEWORK;
+
+public class Hashset_01 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ COLLECTION_FRAMEWORK/Set_01.java
@@ -0,0 +1,7 @@
+package COLLECTION_FRAMEWORK;
+
+public class Set_01 {
+    public static void main(String[] args) {
+        
+    }
+}
